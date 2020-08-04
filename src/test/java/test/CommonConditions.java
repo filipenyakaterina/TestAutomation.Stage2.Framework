742371cefed18453d1cd57c033207af29b32e919
Ekaterina Filipenya <katerina.filipenua@gmail.com>
2020-08-04 22:19:51 +0300

    remove setLogger

--- src/test/java/test/CommonConditions.java
+++ src/test/java/test/CommonConditions.java
@@ -3,7 +3,6 @@ package test;
 import driver.DriverSingleton;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import utils.TestListener;
@@ -13,11 +12,6 @@ public class CommonConditions {
     protected static final String SEARCH_QUERY = "Google Cloud Platform Pricing Calculator";
 
     protected WebDriver driver;
-
-    @BeforeClass
-    public static void setLogger(){
-        System.getProperty("log4j.configurationFile", "log4j.xml");
-    }
 
     @BeforeMethod(alwaysRun = true)
     public void browserSetup() {
